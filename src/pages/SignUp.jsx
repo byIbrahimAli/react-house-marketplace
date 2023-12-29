d7ed190844b8ea3d2e37edebe1e7aaaac9c9597f
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2023-12-29 09:06:37 +0000

    sign in and sign up pages complete. Pretty crazy how fast functionality can be made when you have the CSS already sorted.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,91 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignUp() {
-  return <h1>Sign Up</h1>
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+  const { name, email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value, // updates whatever id was in target, smart!
+      // why is id in []?
+      // when using a dynamic value as a property name, you gotta use []
+      // tells JS to evaluate expression inside brackets and use result as property name
+    }))
+  }
+
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+
+        <main>
+          <form>
+            <input
+              type='text'
+              className='nameInput'
+              placeholder='Name'
+              id='name'
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='email'
+              className='emailInput'
+              placeholder='Email'
+              id='email'
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'test' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+
+            <img
+              src={visibilityIcon}
+              alt='show password'
+              className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password?
+          </Link>
+
+          <div className='signUpBar'>
+            <p className='signUpText'>Sign Up</p>
+            <button className='signUpButton'>
+              {<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />}
+            </button>
+          </div>
+        </main>
+        {/* Google OAuth */}
+        <Link to='/sign-in' className='registerLink'>
+          Sign In Instead
+        </Link>
+      </div>
+    </>
+  )
 }
 export default SignUp
