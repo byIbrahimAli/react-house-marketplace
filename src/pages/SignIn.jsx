b6d6e70565fc9eea0571ed78d75ee1b3323916f0
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2023-12-30 05:47:45 +0000

    we registered a fucking user!! pretty cool. minor mistake, I closed the form too early, so the onSubmit never got called..

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -40,36 +40,36 @@ function SignIn() {
               value={email}
               onChange={onChange}
             />
+
+            <div className='passwordInputDiv'>
+              <input
+                type={showPassword ? 'test' : 'password'}
+                className='passwordInput'
+                placeholder='Password'
+                id='password'
+                value={password}
+                onChange={onChange}
+              />
+
+              <img
+                src={visibilityIcon}
+                alt='show password'
+                className='showPassword'
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            </div>
+
+            <Link to='/forgot-password' className='forgotPasswordLink'>
+              Forgot Password?
+            </Link>
+
+            <div className='signInBar'>
+              <p className='signInText'>Sign In</p>
+              <button className='signInButton'>
+                {<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />}
+              </button>
+            </div>
           </form>
-
-          <div className='passwordInputDiv'>
-            <input
-              type={showPassword ? 'test' : 'password'}
-              className='passwordInput'
-              placeholder='Password'
-              id='password'
-              value={password}
-              onChange={onChange}
-            />
-
-            <img
-              src={visibilityIcon}
-              alt='show password'
-              className='showPassword'
-              onClick={() => setShowPassword((prevState) => !prevState)}
-            />
-          </div>
-
-          <Link to='/forgot-password' className='forgotPasswordLink'>
-            Forgot Password?
-          </Link>
-
-          <div className='signInBar'>
-            <p className='signInText'>Sign In</p>
-            <button className='signInButton'>
-              {<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />}
-            </button>
-          </div>
         </main>
         {/* Google OAuth */}
         <Link to='/sign-up' className='registerLink'>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -24,6 +30,32 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      // registering user
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      const user = userCredentials.user
+
+      // updating display name
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -32,7 +64,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type='text'
               className='nameInput'
@@ -49,36 +81,36 @@ function SignUp() {
               value={email}
               onChange={onChange}
             />
+
+            <div className='passwordInputDiv'>
+              <input
+                type={showPassword ? 'test' : 'password'}
+                className='passwordInput'
+                placeholder='Password'
+                id='password'
+                value={password}
+                onChange={onChange}
+              />
+
+              <img
+                src={visibilityIcon}
+                alt='show password'
+                className='showPassword'
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            </div>
+
+            <Link to='/forgot-password' className='forgotPasswordLink'>
+              Forgot Password?
+            </Link>
+
+            <div className='signUpBar'>
+              <p className='signUpText'>Sign Up</p>
+              <button className='signUpButton'>
+                {<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />}
+              </button>
+            </div>
           </form>
-
-          <div className='passwordInputDiv'>
-            <input
-              type={showPassword ? 'test' : 'password'}
-              className='passwordInput'
-              placeholder='Password'
-              id='password'
-              value={password}
-              onChange={onChange}
-            />
-
-            <img
-              src={visibilityIcon}
-              alt='show password'
-              className='showPassword'
-              onClick={() => setShowPassword((prevState) => !prevState)}
-            />
-          </div>
-
-          <Link to='/forgot-password' className='forgotPasswordLink'>
-            Forgot Password?
-          </Link>
-
-          <div className='signUpBar'>
-            <p className='signUpText'>Sign Up</p>
-            <button className='signUpButton'>
-              {<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />}
-            </button>
-          </div>
         </main>
         {/* Google OAuth */}
         <Link to='/sign-in' className='registerLink'>
