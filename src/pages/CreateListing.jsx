22a5d2576853be97ce6dd7da7292d4147c71fa4f
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-01-09 05:50:48 +0000

    Minor fix as api can sometimes return an address that does not match address entered - it misses the door number.

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -113,7 +113,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
     }
 
     // store image in firebase
@@ -177,9 +176,9 @@ function CreateListing() {
       timestamp: serverTimestamp(),
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
