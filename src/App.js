9da99b18da5eb026ee02d8e9583ba3876848c714
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-01-13 11:27:02 +0000

    Contact page done. Sends an email to landlord.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
             path='/category/:categoryName/:listingId'
             element={<Listing />}
           />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
